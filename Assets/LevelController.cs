59fbc9137bd0bb53685e21dac73b4807aaf66ddd
Joseph Yannessa <baconcheese113@hotmail.com>
2020-11-11 01:59:10 -0700

    Levels

---
+++ Assets/LevelController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+  private Enemy[] _enemies;
+  private static int _nextLevelIndex = 1;
+
+  private void OnEnable()
+  {
+    _enemies = FindObjectsOfType<Enemy>();
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    foreach (Enemy enemy in _enemies)
+    {
+      if (enemy)
+      {
+        return;
+      }
+    }
+
+    print("Level finished");
+    string nextLevelName = "Level" + ++_nextLevelIndex;
+    SceneManager.LoadScene(nextLevelName);
+  }
+}
