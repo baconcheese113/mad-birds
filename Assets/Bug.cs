1a7f998c61f6f896ff2da7ef7a16ed064afe64a1
Joseph Yannessa <baconcheese113@hotmail.com>
2020-11-10 22:20:20 -0700

    Reset bug if player is trash

--- Assets/Bug.cs
+++ Assets/Bug.cs
@@ -1,12 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bug : MonoBehaviour
 {
   private Vector3 _initialPosition;
+  private bool _birdWasLaunched;
+  private float _timeSittingAround = 0;
+
+  [SerializeField] private float _launchPower = 600;
 
   private void Awake()
   {
     _initialPosition = transform.position;
+  }
+
+  private void Update()
+  {
+    if (_birdWasLaunched && GetComponent<Rigidbody2D>().velocity.magnitude <= .1)
+    {
+      _timeSittingAround += Time.deltaTime;
+    }
+    if (_timeSittingAround > .5 || Math.Abs(transform.position.y) > 10 || Math.Abs(transform.position.x) > 10)
+    {
+      string currentSceneName = SceneManager.GetActiveScene().name;
+      SceneManager.LoadScene(currentSceneName);
+    }
   }
 
   private void OnMouseDown()
@@ -19,8 +38,10 @@ public class Bug : MonoBehaviour
     GetComponent<SpriteRenderer>().color = Color.white;
 
     Vector2 directionToInitialPosition = _initialPosition - transform.position;
-    GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * 100);
+    GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * _launchPower);
     GetComponent<Rigidbody2D>().gravityScale = 1;
+    _timeSittingAround = 0;
+    _birdWasLaunched = true;
   }
 
   private void OnMouseDrag()
