f1b5d0a70e11d068b726d92ad593127168677c79
Joseph Yannessa <baconcheese113@hotmail.com>
2020-11-10 21:24:57 -0700

    init commit

---
+++ Assets/Bug.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Bug : MonoBehaviour
+{
+  private void OnMouseDown()
+  {
+    GetComponent<SpriteRenderer>().color = Color.red;
+  }
+  private void OnMouseUp()
+  {
+    GetComponent<SpriteRenderer>().color = Color.white;
+  }
+  private void OnMouseDrag()
+  {
+    Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    transform.position = new Vector3(newPosition.x, newPosition.y, 0);
+  }
+}
