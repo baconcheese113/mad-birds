7ff25004695e2d74c942f08ee6bee505da13e36b
Joseph Yannessa <baconcheese113@hotmail.com>
2020-11-10 23:45:32 -0700

    Killing on bug touch

---
+++ Assets/Corona/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+  private void OnCollisionEnter2D(Collision2D other)
+  {
+    Bug bug = other.collider.GetComponent<Bug>();
+    if (bug != null)
+    {
+      Destroy(gameObject);
+    }
+  }
+}
