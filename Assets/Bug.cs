b30b39f9304e668b3b6e7cafd86691686d617f60
Joseph Yannessa <baconcheese113@hotmail.com>
2020-11-10 22:01:47 -0700

    Allow Bug to be launched

--- Assets/Bug.cs
+++ Assets/Bug.cs
@@ -2,14 +2,27 @@
 
 public class Bug : MonoBehaviour
 {
+  private Vector3 _initialPosition;
+
+  private void Awake()
+  {
+    _initialPosition = transform.position;
+  }
+
   private void OnMouseDown()
   {
     GetComponent<SpriteRenderer>().color = Color.red;
   }
+
   private void OnMouseUp()
   {
     GetComponent<SpriteRenderer>().color = Color.white;
+
+    Vector2 directionToInitialPosition = _initialPosition - transform.position;
+    GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * 100);
+    GetComponent<Rigidbody2D>().gravityScale = 1;
   }
+
   private void OnMouseDrag()
   {
     Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
