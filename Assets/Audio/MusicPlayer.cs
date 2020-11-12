87bfb201afb527df820192398c20891be78b70a6
Joseph Yannessa <baconcheese113@hotmail.com>
2020-11-12 04:14:18 -0700

    Music player for level music throughout the trilogy

---
+++ Assets/Audio/MusicPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+  [SerializeField] private AudioSource _levelMusic;
+  void Start()
+  {
+    if (_levelMusic) _levelMusic.Play();
+    DontDestroyOnLoad(gameObject);
+  }
+}
---
+++ Assets/ButtonFX.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ButtonFX : MonoBehaviour
+{
+  public AudioSource myFx;
+  public AudioClip hoverFx;
+  public AudioClip clickFx;
+
+  public void HoverSound()
+  {
+    myFx.PlayOneShot(hoverFx);
+  }
+  public void ClickSound()
+  {
+    myFx.PlayOneShot(clickFx);
+  }
+}
