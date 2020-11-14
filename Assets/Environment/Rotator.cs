8488259540d4adb37dd039017bc93b6af0b0b2e1
Joseph Yannessa <baconcheese113@hotmail.com>
2020-11-14 15:07:14 -0700

    Rotator component

---
+++ Assets/Environment/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+  [SerializeField] private float _degsPerSec = 45f;
+
+  // Update is called once per frame
+  void Update()
+  {
+    transform.Rotate(new Vector3(0, 0, _degsPerSec * Time.deltaTime));
+  }
+}
