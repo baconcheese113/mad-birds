e4538a5ee5287b39fdac0fc0223fd9dbfe229366
Joseph Yannessa <baconcheese113@hotmail.com>
2020-11-13 22:49:13 -0700

    AudioPlayer component and SoundCue asset

---
+++ Assets/Audio/AudioPlayer.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+enum PlayMethod
+{
+  RANDOM,
+  CONSECUTIVE
+}
+
+public class AudioPlayer : MonoBehaviour
+{
+  [SerializeField] private SoundCue _soundCue;
+  [SerializeField] private PlayMethod _playMethod = PlayMethod.RANDOM;
+  [SerializeField] private bool _playOnAwake = true;
+  [SerializeField] private float _pitchMin = 1f;
+  [SerializeField] private float _pitchMax = 1f;
+
+  private bool _shouldBePlaying = false;
+  private int _lastPlayedIdx = -1;
+  private AudioSource _audioSource;
+
+  private void OnEnable()
+  {
+    _audioSource = gameObject.AddComponent<AudioSource>();
+  }
+
+  private void Start()
+  {
+    if (_playOnAwake)
+    {
+      _shouldBePlaying = true;
+      _audioSource.Play();
+    }
+  }
+
+  private void Update()
+  {
+    if (_shouldBePlaying && !_audioSource.isPlaying) Play();
+  }
+
+  public void Play()
+  {
+    // print("Trying to play");
+    int slotIdx = GetNextSlotIdx();
+    print("slotIdx is " + slotIdx);
+    if (slotIdx == -1) return;
+    AudioSlot slot = _soundCue._audioSlots[slotIdx];
+    _audioSource.clip = slot._audioClip;
+    _audioSource.volume = slot._volume;
+    _audioSource.pitch = Random.Range(_pitchMin, _pitchMax);
+    _audioSource.Play();
+    print("Playing on audioSource");
+    _lastPlayedIdx = slotIdx;
+  }
+
+  private int GetNextSlotIdx()
+  {
+    print("soundCue is " + !!_soundCue);
+    if (!_soundCue || _soundCue._audioSlots.Count == 0) return -1;
+    int randIdx = Random.Range(0, _soundCue._audioSlots.Count);
+    int nextIdx = _playMethod == PlayMethod.RANDOM ? randIdx : _lastPlayedIdx + 1;
+    return nextIdx;
+  }
+}
---
+++ Assets/Audio/SoundCue.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+// For ordering https://blog.redbluegames.com/guide-to-extending-unity-editors-menus-b2de47a746db#62b3 (⊙_ ☉)
+[CreateAssetMenu(fileName = "SoundCue", menuName = "SoundCue", order = 216)]
+public class SoundCue : ScriptableObject
+{
+  public List<AudioSlot> _audioSlots;
+}
+
+[System.Serializable]
+public class AudioSlot
+{
+  public AudioClip _audioClip;
+  [Range(0f, 2f)]
+  public float _volume = 1f;
+
+  private void OnEnable()
+  {
+    _volume = 1f;
+  }
+}
