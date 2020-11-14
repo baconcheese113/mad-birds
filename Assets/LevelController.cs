3101f8e3b5e818f9549d7a66fdc62529b11bbdda
Joseph Yannessa <baconcheese113@hotmail.com>
2020-11-14 04:31:34 -0700

    Level UI added with enemies left slider and level text

--- Assets/LevelController.cs
+++ Assets/LevelController.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelController : MonoBehaviour
 {
   [SerializeField] private int _totalLevels = 3;
+  [SerializeField] private TextMeshProUGUI _levelText;
+  [SerializeField] private Slider _enemiesSlider;
 
+  private int _enemiesStart;
   private List<Enemy> _enemiesLeft;
   private static int _nextLevelIndex = 1;
   private bool _levelFinished = false;
@@ -16,6 +21,10 @@ public class LevelController : MonoBehaviour
   {
     Enemy[] enemies = FindObjectsOfType<Enemy>();
     _enemiesLeft = new List<Enemy>(enemies);
+    _enemiesStart = _enemiesLeft.Count;
+    _enemiesSlider.value = 0;
+    _enemiesSlider.maxValue = _enemiesStart;
+    _levelText.SetText("Level " + _nextLevelIndex + " / " + _totalLevels);
     CinemachineTargetGroup targetGroup = FindObjectOfType<CinemachineTargetGroup>();
     foreach (Enemy e in enemies)
     {
@@ -28,6 +37,7 @@ public class LevelController : MonoBehaviour
   public void NotifyEnemyDestroyed(Enemy enemy)
   {
     _enemiesLeft.Remove(enemy);
+    _enemiesSlider.value = _enemiesStart - _enemiesLeft.Count;
     if (_enemiesLeft.Count == 0) FinishLevel();
   }
 
@@ -41,8 +51,14 @@ public class LevelController : MonoBehaviour
       yield return new WaitForSeconds(1.5f);
       _nextLevelIndex = Mathf.Min(_nextLevelIndex + 1, _totalLevels);
       string nextLevelName = "Level" + _nextLevelIndex;
+      print("Loading " + nextLevelName);
       SceneManager.LoadScene(nextLevelName);
     }
     StartCoroutine(ProceedToNextLevel());
+  }
+
+  public void ShowPauseMenu()
+  {
+
   }
 }
